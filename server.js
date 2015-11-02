419611eab7bb1b45c471c14e269c7fd4bbe3868e
Marius <mariusjohans@gmail.com>
2015-11-02 09:16:27 +0100

    installed expres and jade, added a index view

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-﻿var http = require('http');
+﻿var express = require('express');
+var app = express();
+
+app.set("view engine", "jade");
+
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+
+app.get("/", function (req, res) {   
+    res.render("index");
+});
+
+app.listen(port)
+
+//var http = require('http');
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
